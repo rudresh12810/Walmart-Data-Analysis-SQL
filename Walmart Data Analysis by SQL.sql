07f7f06760db93e643d77aa3813079bed2ff136c
Rudresh Patidar <76532612+rudresh12810@users.noreply.github.com>
2024-05-27 08:53:40 +0530

    Add files via upload

---
+++ Walmart Data Analysis by SQL.sql
@@ -0,0 +1,27 @@
+Create Database Walmart;
+
+Use Walmart;
+
+-- drop table sales;
+
+CREATE TABLE IF NOT EXISTS sales(
+	invoice_id VARCHAR(30) NOT NULL PRIMARY KEY,
+    branch VARCHAR(5) NOT NULL,
+    city VARCHAR(30) NOT NULL,
+    customer_type VARCHAR(30) NOT NULL,
+    gender VARCHAR(30) NOT NULL,
+    product_line VARCHAR(100) NOT NULL,
+    unit_price DECIMAL(10,2) NOT NULL,
+    quantity INT NOT NULL,
+    VAT FLOAT(6,4) NOT NULL,
+    total DECIMAL(12, 4) NOT NULL,
+    date DATETIME NOT NULL,
+    time TIME NOT NULL,
+    payment_method VARCHAR(15) NOT NULL,
+    cogs DECIMAL(10,2) NOT NULL,
+    gross_margin_pct FLOAT(11,9),
+    gross_income DECIMAL(12, 4) NOT NULL,
+    rating FLOAT(2, 1)
+);
+
+select * from sales;
